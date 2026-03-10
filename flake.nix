f0dd443009411de0866e7d4b24c0a391db56f600
AKSizov <37084670+AKSizov@users.noreply.github.com>
2026-03-10 19:39:30 -0400

    fix: arch

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
         mininix = mkHost "x86_64-linux" ./hosts/mininix.nix;
         m1n1x = mkHost "aarch64-linux" ./hosts/m1n1x.nix;
         closetcard = mkHost "x86_64-linux" ./hosts/closetcard.nix;
-        grassblock = mkHost "x86_64-linux" ./hosts/grassblock.nix;
+        grassblock = mkHost "aarch64-linux" ./hosts/grassblock.nix;
       };
     };
 }
