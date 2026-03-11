6db524f63440197573db9779f1ac772087947a1f
AKSizov <37084670+AKSizov@users.noreply.github.com>
2026-03-10 22:55:53 -0400

    new: disable firewall on grassblock.

--- hosts/grassblock.nix
+++ hosts/grassblock.nix
@@ -16,6 +16,7 @@
   systemd.targets.hybrid-sleep.enable = false;
 
   boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
+  networking.firewall.enable = false;
 
   boot.loader.systemd-boot.enable = false;
   boot.loader.efi = {
