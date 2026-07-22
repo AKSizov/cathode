924c12653a281d5f58b0e845255d657c614913c3
Kiera <kiera@cathodelabs.com>
2026-07-22 02:23:26 +0000

    flake: switch from nixos-unstable to nixos-26.05 stable
    - nixpkgs: nixos-unstable → nixos-26.05
    - home-manager: master → release-26.05
    - zen-browser: remove nixpkgs/home-manager follows (needs unstable for Firefox compat)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Cathode - A clean NixOS distribution";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -31,10 +31,6 @@
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake/beta";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-      };
     };
   };
 
